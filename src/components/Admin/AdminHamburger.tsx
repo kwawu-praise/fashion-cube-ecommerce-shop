f9550428664c516a71083f0f978d1b053aa0514e
kwawu-praise <kwawupraise22@gmail.com>
2026-03-31 19:43:32 +0000

    added css to the Admin Hamburger Menu

--- src/components/Admin/AdminHamburger.tsx
+++ src/components/Admin/AdminHamburger.tsx
@@ -7,7 +7,8 @@ import { MdMessage } from "react-icons/md";
 import { FaBell } from "react-icons/fa";
 import { IoSettingsSharp } from "react-icons/io5";
 import { IoPersonSharp } from "react-icons/io5";
-
+import { IoIosArrowForward } from "react-icons/io";
+import { IoMdLogOut } from "react-icons/io";
 
 
 interface AdminHamburgerInteface {
@@ -66,18 +67,28 @@ interface HamburgerItemProps extends AdminHamburgerInteface {
 const HamburgerItem = ({ title, subItems, icon: Icon, isOpen, onToggle }: HamburgerItemProps,) => {
   return (
 
-    <div>
-      <div>
-        <Icon></Icon>
-        <button className="border" onClick={onToggle}>
-          {title}
+    <div className="mt-3">
+      <div className="flex px-2 gap-2  ">
+        <button className={` rounded-2xl p-4 h-12 font-bold hover:bg-[#f1efee]  w-full flex items-center justify-between ${isOpen ? "bg-[#f1efee] " : "bg-white"} `} onClick={onToggle}>
+          <div className="flex items-center gap-2 text-[#635f74] ">
+            <Icon className={`${isOpen ? "bg-[#f1efee] text-blue-700" : "bg-white"}`} />
+            {title}
+
+          </div>
+          <div>
+            {
+              subItems &&
+              <IoIosArrowForward className={`font-bold ${isOpen ? "rotate-90" : ""} `} />
+            }
+
+          </div>
         </button>
       </div>
       {
-        subItems && <div className={`grid transition-all duration-300 ${isOpen ? "grid-rows-[1fr]" : "grid-rows-[0fr]"}`}>
-          <ul className="overflow-hidden">
+        subItems && <div className={` overflow-hidden grid transition-all duration-300 ${isOpen ? "grid-rows-[1fr]" : "grid-rows-[0fr]"} pl-10`}>
+          <ul className=" overflow-hidden border-l p-3 space-y-3">
             {subItems?.map((item) => (
-              <li key={item}>{item}</li>
+              <li key={item} className="text-sm text-[#656275] font-bold hover:text-blue-700">{item}</li>
             ))}
           </ul>
         </div>
@@ -101,14 +112,32 @@ const AdminHamburger = () => {
 
 
   return (
-    <div>
+    <div className="w-full h-screen">
 
-      <main>
-        {
-          AdminHamburgerItems.map(({ title, subItems, icon: Icon, }, index) => (
-            <HamburgerItem title={title} subItems={subItems} icon={Icon} onToggle={() => handleToggle(title)} isOpen={isOpen === title} />
-          ))
-        }
+      <main className="w-[20%]  h-full flex flex-col justify-between">
+        <div>
+          {
+            AdminHamburgerItems.map(({ title, subItems, icon: Icon, }) => (
+              <HamburgerItem title={title} subItems={subItems} icon={Icon} onToggle={() => handleToggle(title)} isOpen={isOpen === title} />
+            ))
+          }
+
+
+        </div>
+        <div className="px-4  mb-40">
+          <div className="w-full flex items-center justify-center  bg-[#bacff7] h-30 relative overflow-hidden rounded-xl">
+            <button className="bg-blue-600 h-10 w-30 rounded-sm text-white font-bold flex items-center justify-center gap-1" >
+              <IoMdLogOut />
+              LOGOUT
+            </button>
+            <div className="w-5 h-5 rounded-full bg-[#5f91f2] absolute left-1 top-3">
+            </div>
+
+            <div className="w-10 h-10 rounded-full bg-[#5f91f2] absolute right-[-5%] top-[-6%]">
+            </div>
+          </div>
+
+        </div>
       </main>
     </div>
   )
